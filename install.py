ce016bacad7724bd20416eaf55593060d22c7bc0
hightechharmony <smcgrath@nfatech.com>
2025-04-09 23:43:48 -0400

    Added API service unit creation to installer.  Updated config file example.

--- install.py
+++ install.py
@@ -23,7 +23,7 @@ def read_config():
     config = SimpleNamespace(**config_dict)
     
 
-def create_service_file():
+def create_showrecorder_service_file():
     print(f"Creating service file for {config.runasuser} and {config.runasgroup}")
     service_content = f"""[Unit]
 Description=Show Recorder Service
@@ -47,20 +47,54 @@ WantedBy=multi-user.target
         f.write(service_content)
         
 
-
+def create_api_service_file():
+    print(f"Creating service file for {config.runasuser} and {config.runasgroup}")
+    service_content = f"""[Unit]
+Description=Show Recorder API Service
+After=network.target
+[Service]
+Type=simple 
+ExecStart=/usr/bin/python3 {config.home_dir}/show_server_api.py
+WorkingDirectory={config.home_dir}
+User={config.runasuser}
+Group={config.runasgroup}
+StandardOutput=journal
+StandardError=journal
+Restart=always
+RestartSec=5
+[Install]
+WantedBy=multi-user.target
+"""
+    # Write this out
+    with open("show_recorder_api.service", "w") as f:
+        f.write(service_content)
 
 def main():
     read_config()
-    create_service_file()
+    create_showrecorder_service_file()
+    create_api_service_file()
+    # Check if the user is root
+    if subprocess.run(["id", "-u"]).returncode != 0:
+        print("This script must be run as root.")
+        return
 
     service_file_path = "/etc/systemd/system/"
 
     # Copy the service file to the systemd directory
     print(f"Copying service file to {service_file_path}")
     subprocess.run(["sudo", "cp", "show_recorder.service", service_file_path], check=True)
+    subprocess.run(["sudo", "cp", "show_recorder_api.service", service_file_path], check=True)
     subprocess.run(["sudo", "systemctl", "daemon-reload"], check=True)
     subprocess.run(["sudo", "systemctl", "enable", "show_recorder.service"], check=True)
+    subprocess.run(["sudo", "systemctl", "enable", "show_recorder_api.service"], check=True)
     subprocess.run(["sudo", "systemctl", "start", "show_recorder.service"], check=True)
+    subprocess.run(["sudo", "systemctl", "start", "show_recorder_api.service"], check=True)
+
+    print("Service files created and started successfully.")
+    print("The API should be accessible at http://localhost:5000")
+    # print("The web UI should be accessible at")
+    print("Please check the status of the service with 'systemctl status show_recorder.service' and 'systemctl status show_recorder_api.service'.")
+    print("Happy Recording!")
 
 if __name__ == "__main__":
     main()
